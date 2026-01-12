336424357ea46338da75f43d65e263a9ef3e55af
Herb Vanselow <herbv@nait.ca>
2026-01-12 09:53:39 -0700

    demo01 initial

---
+++ GenericDemo/GenericDemo/ExtLib.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericDemo
+{
+  public static class ExtLib
+  {
+    public static IEnumerable<(int ind, T val)> enumerate<T>( this IEnumerable<T> lst )
+    {
+      List<(int ind, T val)> result = new();
+      
+      int index = 0;
+      foreach (T item in lst)
+        result.Add(( index++, item ));
+      return result;
+    }
+  }
+}
---
+++ GenericDemo/GenericDemo/Program.cs
@@ -0,0 +1,24 @@
+﻿using static System.Console;
+namespace GenericDemo
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      { // extension method version
+        List<double> dubs = [1.2, 2.4, 56.6];
+        foreach (var item in dubs.enumerate())
+        {
+          WriteLine($"{item.ind} : {item.val}");
+        }
+      }
+      { // old argument version
+        List<char> dubs = ['A', 'C', 'Z'];
+        foreach (var item in ExtLib.enumerate(dubs))
+        {
+          WriteLine($"{item.ind} : {item.val}");
+        }
+      }
+    }
+  }
+}
