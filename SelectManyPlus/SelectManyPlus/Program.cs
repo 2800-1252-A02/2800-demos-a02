3f65d0ec7f1a4bed0ce2d1b4ecc9acb6d245deea
Herb Vanselow <herbv@nait.ca>
2026-01-30 12:26:50 -0700

    Select with new{}

--- SelectManyPlus/SelectManyPlus/Program.cs
+++ SelectManyPlus/SelectManyPlus/Program.cs
@@ -1,4 +1,4 @@
-﻿// SelectMany Demo - See bottom of file for TestObj class
+﻿// SelectMany Demo - See bottom of file for TestObj class --------------------------------
 
 // List of TestObj which contains a internal exposed collection we want to extract,
 //  - so NESTED DATA is what we want
@@ -30,11 +30,12 @@ var flattenedWithParent = hierarchicalCollection.SelectMany(
 
 foreach (var str in flattenedWithParent)
   Console.WriteLine(str);
+Console.WriteLine();
 
 // The result is an IEnumerable<string> containing:
 // "t1 : Item1", "t1 : Item2", "t2 : Item3", "t2 : Item4"
 
-Console.WriteLine();
+// GroupBy Demo - grouping a string's chars by uppercase or not ----------------------------
 
 var stringAsCollection = "I WonDer What TomorRow will BRING to ME ? A puppy mAybe ?";
 Console.WriteLine($"Using : \u001b[43m\u001b[34m{stringAsCollection}\u001b[0m, grouping by isUpper() return : ");
@@ -50,6 +51,44 @@ foreach ( var groupItem in stringAsCollection.GroupBy( ch => char.IsUpper(ch)))
 Console.WriteLine();
 
 
+// Select with new()
+// See : https://learn.microsoft.com/en-us/dotnet/csharp/fundamentals/types/anonymous-types
+
+// Create anonymous types on the fly with selected properties using  : new { prop1 = val1, prop2 = val2 }
+var anonThing = new { Id = 123, Name = "Beavis", Score = 9.3 }; // anonymous type, hover over anonThing to see type details
+Console.WriteLine($"Anonymous Type created with new {{ }} : Id={anonThing.Id}, Name={anonThing.Name}, Score={anonThing.Score}");
+
+// using this feature, you can capture local variables, or use lambda parameters to create new objects on the fly
+var anonSelected = hierarchicalCollection.Select( t => 
+    new 
+    { 
+        TheName = t.Name, 
+        ItemCount = t.Items.Count, 
+        FirstItem = t.Items.FirstOrDefault() 
+    } 
+);
+foreach ( var anon in anonSelected)
+{
+  Console.WriteLine($"Anon Selected : TheName={anon.TheName}, ItemCount={anon.ItemCount}, FirstItem={anon.FirstItem}");
+}
+
+// You must specify an anonymous type name if values are not from variables or existing properties, but
+//   it will auto-infer the property types from the assigned member/variable names.
+var SecretNumber = 42;
+var anonSelectedUnnamed = hierarchicalCollection.Select(t =>
+    new
+    {
+      t.Name,
+      t.Items.Count,
+      //t.Items.FirstOrDefault(), not allowed, needs a var/prop name
+      SecretNumber, // OK, but you can't perform operations like SecretNumber++, without specifying a name as below
+      BiggerSecret = SecretNumber++ * 1000 // OK, new name
+    }
+);
+foreach (var anon in anonSelectedUnnamed)
+{
+  Console.WriteLine($"Unnamed : Name={anon.Name}, Count={anon.Count}, SecretNumber={anon.SecretNumber}, BiggerSecret={anon.BiggerSecret}");
+}
 class TestObj
 {
   public string Name { get; set; }
