41f54a1f8f381443636ebc9c081ca817a5e94be6
Herb Vanselow <herbv@nait.ca>
2026-03-19 11:52:33 -0600

    Moved To Self contained Demo

---
+++ ConnectionDialogSolution/ConnectionDialogLibrary/ConDlg.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConnectionDialogLibrary
+{
+  public partial class ConDlg : Form
+  {
+    //DialogResult r = DialogResult.
+    System.Windows.Forms.Timer _tim = new System.Windows.Forms.Timer();
+    public ConDlg()
+    {
+      InitializeComponent();
+      BackColor = Color.Coral;
+      _tim.Interval = 4000;
+      _tim.Tick += (s, e) => { DialogResult = DialogResult.OK;  };
+      _tim.Start();
+    }
+  }
+}
---
+++ ConnectionDialogSolution/ConnectionDialogTestHarness/Form1.cs
@@ -0,0 +1,24 @@
+//using ConnectionDialogLibrary;
+using static System.Diagnostics.Trace;
+
+namespace ConnectionDialogTestHarness
+{
+  public partial class Form1 : Form
+  {
+    public Form1()
+    {
+      InitializeComponent();
+      KeyDown += Form1_KeyDown;
+    }
+
+    private void Form1_KeyDown(object? sender, KeyEventArgs e)
+    {
+      // Cases of trying the Dialog and verifying the result
+      if( e.KeyCode == Keys.Enter )
+      {
+        ConnectionDialogLibrary.ConDlg dlg = new();
+        WriteLine(dlg.ShowDialog());
+      }
+    }
+  }
+}
